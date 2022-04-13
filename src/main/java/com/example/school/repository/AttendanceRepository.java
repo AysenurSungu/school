588ec63a872c4d2e3c0464cc6dd8ab0010f405ca
aysenur-sungu <sungu.aysenur@gmail.com>
2022-04-13 05:23:52 +0300

    repository has been edited

--- src/main/java/com/example/school/repository/AttendanceRepository.java
+++ src/main/java/com/example/school/repository/AttendanceRepository.java
@@ -4,4 +4,5 @@ import com.example.school.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
 }
--- src/main/java/com/example/school/repository/ClassroomRepository.java
+++ src/main/java/com/example/school/repository/ClassroomRepository.java
@@ -4,5 +4,6 @@ import com.example.school.entity.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+
 }
 
